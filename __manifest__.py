8120b3ee6c62e9901bc1fb5d1fed5bfc7e93b0c2
XZA <xzabala@uni.eus>
2021-11-18 21:57:19 +0000

    __manifest__.py zuzenduta modulua aplikazio bezala hartzeko
    	modified:   __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "biki",
-    'application':True
+    'application':True,
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
