59c538d80db29ad7e9493c49c1e8d2e36ffc0514
XZA <xzabala@uni.eus>
2022-03-07 08:10:41 +0000

    Taulak egokituta (aldagai aldaketa) eta motak zuzenduta (5)

--- models/biki_lagina.py
+++ models/biki_lagina.py
@@ -8,11 +8,11 @@ class Lagina(models.Model):
     _description = 'Laginak gordetzeko'
 
     NumMuestra = fields.Integer(index=True,required=True)
-    PosicionReal = fields.integer()
-    VelocidadReal = fields.integer()
-    VelocidadConsigna = fields.integer()
-    CargaReal = fields.integer()
-    CargaContacto = fields.integer()
+    PosicionReal = fields.Integer()
+    VelocidadReal = fields.Integer()
+    VelocidadConsigna = fields.Integer()
+    CargaReal = fields.Integer()
+    CargaContacto = fields.Integer()
     Egoera = fields.Many2one('biki.egoera', string='Egoera', index=True,required=True)
     Exekuzioa = fields.Many2one('biki.exekuzioa', string='Exekuzioa', index=True,required=True)
 
